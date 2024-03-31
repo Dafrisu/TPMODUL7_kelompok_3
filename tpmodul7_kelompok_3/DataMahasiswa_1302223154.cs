36f540107d0c9e8bd47b11f960e1bacec4d29bf0
darrylrambi <darrylrambi@gmail.com>
2024-03-31 22:51:43 +0700

    Menambahkan class DataMahasiswa_1302223154

---
+++ tpmodul7_kelompok_3/DataMahasiswa_1302223154.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+public class DataMahasiswa
+{
+    [JsonPropertyName("nim")]
+    public int Nim { get; set; }
+    [JsonPropertyName("nama")]
+    public Nama name { get; set; }
+    [JsonPropertyName("fakultas")]
+    public string Fakultas { get; set; }
+    public String json = System.IO.File.ReadAllText(@"C:\Users\ASUS\Documents\KPL\TP_MOD7_1302223154_DarrylRambi\TPMODUL7_kelompok_3\tpmodul7_kelompok_3\tp7_1_1302223154.json");
+
+    public class Nama
+    {
+        [JsonPropertyName("depan")]
+        public string Depan { get; set; }
+
+        [JsonPropertyName("belakang")]
+        public string Belakang { get; set; }
+    }
+
+    public void ReadJSON()
+    {
+        Console.WriteLine(json);
+    }
+}
--- tpmodul7_kelompok_3/Program.cs
+++ tpmodul7_kelompok_3/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa mhs = new DataMahasiswa();
+
+        var options = new JsonSerializerOptions
+        {
+            IncludeFields = true,
+        };
+
+        var mahasiswa = JsonSerializer.Deserialize<DataMahasiswa>(mhs.json);
+        Console.WriteLine($"Nama {mahasiswa.name.Depan} {mahasiswa.name.Belakang} dengan NIM {mahasiswa.Nim}" + $" dari Fakultas {mahasiswa.Fakultas}");
+    }
+}
