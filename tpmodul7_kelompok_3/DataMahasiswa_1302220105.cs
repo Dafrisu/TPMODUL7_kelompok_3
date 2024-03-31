62c295102132b30111ee379fdc8f8d0171067e82
Mahesazain <echa.zain@gmail.com>
2024-03-31 21:41:06 +0700

    menambahkan class DataMahasiswa_1302220105

---
+++ tpmodul7_kelompok_3/DataMahasiswa_1302220105.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace tpmodul7_kelompok_3
+{
+    internal class DataMahasiswa_1302220105
+    {
+        public class namaData
+        {
+            public string depan;
+            public string belakang;
+            
+            public namaData(string depan, string belakang)
+            {
+                this.depan = depan;
+                this.belakang = belakang;
+            }
+        }
+        public class MahasiswaData
+        {
+            public namaData nama;
+            public int nim;
+            public string fakultas;
+
+            public MahasiswaData(namaData nama, int nim,string fakultas)
+            {
+                this.nama = nama;
+                this.nim = nim;
+                this.fakultas = fakultas;
+            }
+            public void PrintData()
+            {
+                Console.WriteLine($"Nama {nama.depan} {nama.belakang} dengan nim {nim} dari fakultas {fakultas}");
+            }
+        }
+        public static void ReadJSON()
+        {
+            string fileName = "tp7_1_1302220105.json";
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            try
+            {
+                string jsonData = File.ReadAllText(filePath);
+                MahasiswaData mahasiswaData = JsonConvert.DeserializeObject<MahasiswaData>(jsonData, new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Auto
+                });
+
+                mahasiswaData.PrintData();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading JSON file: {ex.Message}");
+            }
+        }
+    }
+}
--- tpmodul7_kelompok_3/Program.cs
+++ tpmodul7_kelompok_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace tpmodul7_kelompok_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa_1302220105.ReadJSON();
+        }
+    }
+}
